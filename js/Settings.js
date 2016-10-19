a394b22d34e1442008c43ed8e507bdbab63f06b2
asosunoff <al.sosunoff@gmail.com>
2016-10-19 22:56:51 +0300

    Added library Lightbox2, Fixed images people

--- js/Settings.js
+++ js/Settings.js
@@ -3,4 +3,10 @@ $(function(){
     //$(".top_mnu") - это верхняя панель со ссылками.
     //Ссылки вида <a href="#contacts">Контакты</a>
     $(".top_mnu").navigation();
+
+    //http://lokeshdhakar.com/projects/lightbox2/
+    lightbox.option({
+        'albumLabel': "Фотография %1 из %2",
+        'wrapAround': true
+    })
 });
